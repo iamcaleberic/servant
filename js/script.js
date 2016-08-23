0241fb871b0f1906e880aaf08cb69b09bdf870a7
Eric Caleb <calebme@mail.com>
2016-08-23 17:20:21 +0100

    Added activex object support

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-var request = new XMLHttpRequest();
+var request;
+if (window.XMLHttpRequest){
+    request = new XMLHttpRequest();
+
+} else {
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
 request.open('GET' , 'data.txt' , false);
 request.onreadystatechange = function(){
     if ((request.readyState === 4) && (request.status === 200)){
