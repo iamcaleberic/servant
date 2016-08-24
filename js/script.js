ed689bf1dba23a537ae7edb4f5eb0261ba17abbd
Eric Caleb <calebme@mail.com>
2016-08-24 06:04:23 +0100

    Parse XML data with AJAX

--- js/script.js
+++ js/script.js
@@ -15,3 +15,30 @@ request.onreadystatechange = function(){
 
 }
 request.send();
+
+var say;
+if (window.XMLHttpRequest){
+    say = new XMLHttpRequest();
+
+} else {
+    say = new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+say.open('GET' , 'data.xml' );
+say.onreadystatechange = function(){
+    if ((say.readyState === 4) && (say.status === 200)){
+        console.log(say.responseXML.getElementsByTagName('name')[1].childNodes[0].nodeValue);
+    }
+
+    var items =  say.responseXML.getElementsByTagName('name');
+    var output = '<ul>' ;
+    for (var i = 0 ; i < items.length; i++){
+        output += '<li>' +
+            items[i].firstChild.nodeValue + '</li>';
+    } 
+    output += '</ul>';
+        document.getElementById('use').innerHTML = output; 
+}
+say.send();
+
+
