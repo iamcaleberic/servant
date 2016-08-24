0d227c0fe52a1db30a952d89a13cf854c527caae
Eric Caleb <calebme@mail.com>
2016-08-24 10:49:34 +0100

    Fixed ui and elements

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(search).keyup(function(){
     $.each(data, function(key ,val ){
         if ((val.name.search(myExp) != -1 ) || (val.bio.search(myExp) != -1 )){
             output += '<li>';
-            output += '<img src="images/me.jpg" height =150 width=150 + alt="' + val.name + '"/>'   
+            output += '<img class="ui avatar image" src="images/me.jpg" height =150 width=150 + alt="' + val.name + '"/>'   
             output += '<h2>' + val.name + '</h2>';
             output += '<p>' + val.bio + '</p>';
             output += '</li>';
