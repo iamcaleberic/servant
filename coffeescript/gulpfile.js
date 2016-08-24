ffa16f0b69f6cb08e86c13eaabcd7727e3981348
Eric Caleb <calebme@mail.com>
2016-08-24 14:00:22 +0100

    Refactor gulp tasks

--- coffeescript/gulpfile.js
+++ coffeescript/gulpfile.js
@@ -1,11 +1,18 @@
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
     uglify= require('gulp-uglify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    livereload = require('gulp-livereload'),
+    lr = require('tiny-lr') ;
+    server = lr();
+
+var jsSources = ['components/scripts/scriptOne.js',
+                 'components/scripts/scriptTwo.js'
+                ];
+
+
 gulp.task('js', function(){
-    gulp.src(['components/scripts/scriptOne.js',
-              'components/scripts/scriptTwo.js'
-    ])
+    gulp.src(jsSources)
     .pipe(uglify())
     .pipe(concat('script.js'))
     .pipe(gulp.dest('js'));
