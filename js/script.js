b6e8c5f6e0ad737bfc05b2287b3c3c9d9c2cb755
Eric Caleb <calebme@mail.com>
2016-08-24 09:15:35 +0100

    Added javascript for bio and image

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$.getJSON('data.json', function(data){
+    var output = '<ul class= "searchresults"> ';
+    $.each(data, function(key ,val ){
+        output += '<li>';
+        output += '<img src="images/me.jpg" height =150 width=150 + alt="' + val.name + '"/>'   
+        output += '<h2>' + val.name + '</h2>';
+        output += '<p>' + val.bio + '</p>';
+        output += '</li>';
+    });
+    output += '</ul>';
+    $('#update').html(output);
+});
