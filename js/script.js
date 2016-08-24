47d13bf5c8876264fc565c21b193952ce9751ee2
Eric Caleb <calebme@mail.com>
2016-08-24 10:21:21 +0100

    Added regex for searching value

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 $(search).keyup(function(){
     var searchField = $('#search').val();
-    console.log(searchField)
+    var myExp = new RegExp(searchField , "i");
+
     $.getJSON('data.json', function(data){
     var output = '<ul class= "searchresults"> ';
     $.each(data, function(key ,val ){
-        output += '<li>';
-        output += '<img src="images/me.jpg" height =150 width=150 + alt="' + val.name + '"/>'   
-        output += '<h2>' + val.name + '</h2>';
-        output += '<p>' + val.bio + '</p>';
-        output += '</li>';
+        if ((val.name.search(myExp) != -1 ) || (val.bio.search(myExp) != -1 )){
+            output += '<li>';
+            output += '<img src="images/me.jpg" height =150 width=150 + alt="' + val.name + '"/>'   
+            output += '<h2>' + val.name + '</h2>';
+            output += '<p>' + val.bio + '</p>';
+            output += '</li>';
+        }
+       
     });
     output += '</ul>';
     $('#update').html(output);
