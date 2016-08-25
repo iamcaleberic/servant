62a1cecf7b3a4f4125706d24717ab4d421df042c
Eric Caleb <calebme@mail.com>
2016-08-25 07:03:50 +0100

    Added html and sass gulp tasks

--- coffeescript/gulpfile.js
+++ coffeescript/gulpfile.js
@@ -1,7 +1,7 @@
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
     uglify= require('gulp-uglify'),
-    sass = require('gulp-ruby-sass')
+    sass = require('gulp-ruby-sass'),
     concat = require('gulp-concat'),
     livereload = require('gulp-livereload'),
     lr = require('tiny-lr') ,
@@ -13,24 +13,34 @@ var jsSources = ['components/scripts/scriptOne.js',
 
 var sassSources = [
     'components/sass/*.scss'
-]
+];
 
 gulp.task('js', function(){
     gulp.src(jsSources)
     .pipe(uglify())
     .pipe(concat('script.js'))
     .pipe(gulp.dest('js'))
+    .pipe(livereload());
 });
 
-gulp.task('sass' , function(){
-    gulp.src(sassSources)
-        .pipe(sass({
-            
-        }))
+gulp.task('html', function(){
+    gulp.src('*.html')
+        .pipe(livereload());
+})
+
+gulp.task('sass', function() {
+  gulp.src(sassSources)
+     return sass(sassSources ,{style: 'expanded', lineNumbers: true} )
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('style'))
+    .pipe(livereload());
 });
 
 gulp.task('watch', function(){
     gulp.watch(jsSources, ['js']);
+    gulp.watch(sassSources,['sass']);
+    gulp.watch('*.html',['html'] )
 });
 
-gulp.task('default', ['js', 'watch']);
+gulp.task('default', [ 'html','sass', 'js', 'watch']);
+    
