3b1e458906476d2da09ef6f79fa9aa3372dc7248
Eric Caleb <calebme@mail.com>
2016-08-25 07:54:31 +0100

    Coffescript dive in

--- coffeescript/components/coffee/script.coffee
+++ coffeescript/components/coffee/script.coffee
@@ -1,2 +1,18 @@
 $ -> 
     $('h1').append("Hello")
+
+
+add = (a ,b) -> 
+    d = 4
+    a + b
+
+# Anonymous or namless func
+do -> 
+    console.log "Servant"
+
+data = 
+    name : "Eric Caleb"
+    numbers : [0,4,4]
+    links:
+        twitter: "https://twitter.com/eric_caleb"
+        github: "https://iamcaleberic.github.io"
--- coffeescript/components/scripts/script.js
+++ coffeescript/components/scripts/script.js
@@ -1,3 +1,24 @@
+var add, data;
+
 $(function() {
   return $('h1').append("Hello");
 });
+
+add = function(a, b) {
+  var d;
+  d = 4;
+  return a + b;
+};
+
+(function() {
+  return console.log("Servant");
+})();
+
+data = {
+  name: "Eric Caleb",
+  numbers: [0, 4, 4],
+  links: {
+    twitter: "https://twitter.com/eric_caleb",
+    github: "https://iamcaleberic.github.io"
+  }
+};
